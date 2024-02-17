97d5890a7779e2114f567a2585d00f3dfcb1e094
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-17 21:38:44 +0530

    Create sizeLL.java

---
+++ sizeLL.java
@@ -0,0 +1,88 @@
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) { // O(1)
+        // step1=create new node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // step2- newNode next=head
+        newNode.next = head; // link
+
+        // step3- head=newNode
+        head = newNode;
+    }
+
+    public void addLast(int data) { // O(1)
+        // step1- create new node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // step2- tail.next=newNode
+        tail.next = newNode;
+
+        // step3- tail=newNode
+        tail = newNode;
+    }
+
+    public void print() { // O(n)
+        if (head == null) { // if we remove this is if operation then null will print
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        // i=idx-1 -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2, 3);
+
+        ll.print();
+        System.out.println(ll.size);
+    }
+}
