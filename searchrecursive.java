7878cb237b649caceb89e9bf75871e8b52ce6e6d
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-17 22:30:40 +0530

    Create searchrecursive.java

---
+++ searchrecursive.java
@@ -0,0 +1,104 @@
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) { // O(1)
+        // step1=create new node
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // step2- newNode next=head
+        newNode.next = head; // link
+
+        // step3- head=newNode
+        head = newNode;
+    }
+
+    public void addLast(int data) { // O(1)
+        // step1- create new node
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // step2- tail.next=newNode
+        tail.next = newNode;
+
+        // step3- tail=newNode
+        tail = newNode;
+    }
+
+    public void print() { // O(n)
+        if (head == null) { // if we remove this is if operation then null will print
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        // i=idx-1 -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int helper(Node head, int key) { //O(n)
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2, 3);
+
+        ll.print();
+
+        System.out.println(ll.recSearch(3));
+        System.out.println(ll.recSearch(10));
+    }
+}
