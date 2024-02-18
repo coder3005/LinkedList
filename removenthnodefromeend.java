122ab707940cd685c2351721fdc9f82d2bf5bae4
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-18 10:04:12 +0530

    Update removenthnodefromeend.java

--- removenthnodefromeend.java
+++ removenthnodefromeend.java
@@ -71,19 +71,6 @@ public class LinkedList {
         temp.next = newNode;
     }
     
-    // public void reverse() { //O(n)
-    //     Node prev=null;
-    //     Node curr=tail=head;
-    //     Node next;
-
-    //     while(curr!=null) {
-    //         next=curr.next;
-    //         curr.next=prev;
-    //         prev=curr;
-    //         curr=next;
-    //     }
-    //     head=prev;
-    // }
     public void deleteNthfromEnd(int n) {
         //calculate size
         int sz=0;
